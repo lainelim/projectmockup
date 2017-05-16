696aba26c573d278382ee415124cdb03d6d54515
hansheng quek <hsquek06@gmail.com>
2017-05-16 14:49:43 +0800

    Functions: pre-test

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,6 @@
-let calculate = {
-
-}
-
 // helper functions
+
+//function that returns the annual salary at nth year (years === nth)
 function myAnnualSalary (years) {
   let monthlySalary = +$('#monthlySalary').value,
     annualIncrement = +$('#annualIncrement').value / 100
@@ -13,12 +11,13 @@ function myAnnualSalary (years) {
 
   return annualSalary
 }
-
+// cpf contribution at nth year
 function myCPFContribution (years) {
   // annual CPF contribution by employee
   return myAnnualSalary(years) * 0.2
 }
 
+//disposableincome at nth year
 function myDisposableIncome (years) {
   // after expenditure
   let expenditure = +$('#expenditure').value
@@ -36,43 +35,108 @@ function myBankSavings (years) {
   let percentageSaved = +$('#savings').value,
       savingsRate = +$('savingsRate').value
 
-  // !!!!!! logic needs refinement !!!!!
   if (years === 0) {
-    return myDisposableIncome(years) * percentageSaved
+    return myDisposableIncome(years) * percentageSaved * (1+savingsRate)
   } else {
-    return (myBankSavings(years - 1) + myDisposableIncome(years)) * percentageSaved
+    return (myBankSavings(years - 1) + (myDisposableIncome(years) * percentageSaved))* (1+savingsRate)
   }
 }
 
-function myEOYSavings () {
-  // returns an array of the cumulative savings (bank) generated each year
-  let startAge = +$('#startAge').value,
-    retireAge = +$('#retireAge').value,
-    deathAge = 100,
-    numWorkingYears = retireAge - startAge,
+function myCPFSavings (years) {
+  let cpfRate = +$('#cpfRate').value
+
+  if (years === 0) {
+    return (myCPFContribution(years) + employerCPFContribution(years)) * (1+cpfRate)
+  } else {
+    return (myCPFSavings(years - 1) + (myCPFContribution(years) + employerCPFContribution(years))) * (1+cpfRate)
+  }
+}
+
+function myCashSavings (years) {
+  let cashRate = +$('#cashRate').value
+      percentageCash = 1 - $('#savings')
+
+  if (years === 0) {
+    return myDisposableIncome(years)* percentageCash * (1+cashRate)
+  } else {
+    return (myCashSavings(years - 1) + (myDisposableIncome(years)*percentageCash)) * (1+cashRate)
+  }
+}
+
+function myTotalNetworth () {
+
+  function mySavingsArray () {
+    // returns an array of the cumulative savings (bank) generated each year
+
+    let numWorkingYears = retireAge - startAge,
     numRetirementYears = deathAge - retireAge,
-    savingsRate = +$('savingsRate').value,
+    savingsRate = +$('#savingsRate').value,
     bankSavingsPerYear = []
 
-  for (var numYears = 0; numYears <= numWorkingYears; i++) {
-    bankSavingsPerYear.push(myBankSavings(numYears))
+    for (var numYears = 0; numYears <= numWorkingYears; i++) {
+      bankSavingsPerYear.push(myBankSavings(numYears))
+    }
+
+    for (var n = 1; n <= numRetirementYears; n++) {
+      bankSavingsPerYear.push(bankSavingsPerYear[bankSavingsPerYear.length-1]*Math.pow(1+savingsRate, n))
+    }
+
+    return bankSavingsPerYear
   }
 
-  for (var n = 1; n <= numRetirementYears; n++) {
-    bankSavingsPerYear.push(bankSavingsPerYear[-1]*Math.pow(1+savingsRate, n))
+  function myCPFArray () {
+
+    let numWorkingYears = retireAge - startAge,
+    numRetirementYears = deathAge - retireAge,
+    cpfRate = +$('#cpfRate').value,
+    cpfSavingsPerYear = []
+
+    for (var numYears = 0; numYears <= numWorkingYears; i++) {
+      cpfSavingsPerYear.push(myCPFSavings(numYears))
+    }
+
+    for (var n = 1; n <= numRetirementYears; n++) {
+      cpfSavingsPerYear.push(cpfSavingsPerYear[cpfSavingsPerYear.length-1]*Math.pow(1+savingsRate, n))
+    }
+
+    return cpfSavingsPerYear
   }
 
-  return bankSavingsPerYear
-}
+  function myCashArray () {
 
-function myEOYCPF () {
+    let numWorkingYears = retireAge - startAge,
+    numRetirementYears = deathAge - retireAge,
+    cashRate = +$('#cashRate').value,
+    accumulatedCashPerYear = []
 
-}
+    for (var numYears = 0; numYears <= numWorkingYears; i++) {
+      accumulatedCashPerYear.push(myCashSavings(numYears))
+    }
 
-function myEOYCash () {
+    for (var n = 1; n <= numRetirementYears; n++) {
+      accumulatedCashPerYear.push(accumulatedCashPerYear[accumulatedCashPerYear.length-1]*Math.pow(1+cashRate, n))
+    }
 
-}
+    return accumulatedCashPerYear
+  }
 
-function myEOYNetworth () {
+  let startAge = +$('#startAge').value,
+      retireAge = +$('#retireAge').value,
+      deathAge = 100,
+      mySavingsArr = mySavingsArray(),
+      myCPFArr = myCPFArray(),
+      myCashArr = myCashArray()
+      dataTable = []
+
+  var totalNetworthArr = mySavingsArr.map(function(val, idx) {
+    return val + myCPFArr[idx] + myCashArr[idx]
+  })
+
+  for (var age=startAge; age <= deathAge; age++) {
+    var yearlyData = [age, totalNetworthArr[age -startAge]]
+    dataTable.push(yearlyData)
+  }
+
+  return dataTable
 
 }
